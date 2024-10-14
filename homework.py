116f7e7993dd5ee67036cf68862a56353cb89340
Maricielo9932 <maricieloramoslopez078@gmail.com>
2024-10-14 10:10:04 -0500

    lun 14 oct 2024 clases

--- homework.py
+++ homework.py
@@ -38,19 +38,47 @@ while True:
         break
     else:
         print("Opción inválida.")
+# CORRECION
+lista_alumnos=[] 
+def mensaje_menu(): 
+  menu_opciones="""
+   --------Bienvenido al Sistema--------
+   ----------Regitra tu alumno----------
+   1. Escribe (s) si deseas agregar un nuevo alumno
+   2. Escribe (n) si deseas salir del sistema 
+   Escribe la Accion que desea realiza:"""
+   return menu_opciones=""
+def ingresar_datos_alumno():  
+  id=len(lista_alumnos)+1 
+  cui=int(input("Ingrese el numero de su dni: ")) 
+  nombre=input("Ingrese el nombre del alumno: ") 
+  apellido input("Ingrese el apeliido del alumno: ") 
+  numero_celular=int(input("Ingrese su numero de celular: ")) 
+  programa_estudio=input("Ingrese el programa de estudio: ") 
+  ciclo_academico=input("Ingrese su ciclo academico: ") 
+  email=input("Ingrese su correo electronico: ") 
+  guardar_datos_alumno(id, cui, nombre, apellido, numero_celular, programa_estudio, ciclo_academico, email)
+def guardar_datos_alumno(id, cui, nombre, apellido, numero_celular, programa_estudio, ciclo_academico,email): 
+  alumno={ 
+    "id":id, 
+    "cui":cui, 
+    "nombre": nombre, 
+    "apellido": apellido,
+    "numero_celular": numero_celular, 
+    "programa_estudio": programa_estudio, 
+    "ciclo_academico":ciclo_academico, 
+    "email":email 
+    ´}
+  lista_alumnos.append(alumno)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+while True: 
+    menu_opciones input(mensaje_sonu())
+    if menu opciones.lower() --"n": 
+      print("Saliendo del Sistema") 
+      break 
+    elif menu opciones.lower() --"s" 
+         Ingresar datos_alumno() 
+    else:
+       print("Opcion erronea") 
+print(lista_alumnos)
+## modularizar
